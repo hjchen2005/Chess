2c47b68f02330363c29a3591ab0a19ee28609dd8
Hj Chen <hjchen2005@gmail.com>
2015-08-11 20:46:31 -0400

    Create UserInterface.java

---
+++ UserInterface.java
@@ -0,0 +1,43 @@
+package com.gottaapp.chess;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class UserInterface extends JPanel implements MouseListener, MouseMotionListener{
+	static int x=0,y=0;
+	@Override
+	public void paintComponent (Graphics g){
+		super.paintComponent(g);
+		this.setBackground(Color.yellow);
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		g.setColor(Color.BLUE); // First layer, because it is drawn befre the purple Rect
+		g.fillRect(x-20,y-20,40,40);
+		g.setColor(new Color(190,81,225));// Purple
+		g.fillRect(40, 20, 80, 50); // Second layer, on top of the blue one
+		g.drawString("Chess", x, y);
+	}
+	@Override
+	public void mouseMoved(MouseEvent e){
+		x=e.getX();
+		y=e.getY();
+		repaint();
+	}
+	@Override
+	public void mousePressed(MouseEvent e){}
+	@Override
+	public void mouseReleased(MouseEvent e){}
+	@Override
+	public void mouseClicked(MouseEvent e){
+		x=e.getX();
+		y=e.getY();
+		repaint();
+	}
+	@Override
+	public void mouseDragged(MouseEvent e){}
+	@Override
+	public void mouseEntered(MouseEvent e){}
+	@Override
+	public void mouseExited(MouseEvent e){}
+}
